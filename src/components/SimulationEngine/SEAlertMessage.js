3b371a356e18e2cb9cda2a2f1d9ad9ce4831f3a5
Edgar KAMDEM <e.kamdem@pickup-services.com>
2024-11-20 14:53:12 +0100

    fix SEAlertMessage

--- src/components/SimulationEngine/SEAlertMessage.js
+++ src/components/SimulationEngine/SEAlertMessage.js
@@ -1,20 +1,32 @@
-'use client';
-
+import React, { useState, useEffect } from "react";
 import ButtonLarge from "../ButtonLarge";
-import React from "react";
 
 const SEAlertMessage = (props) => {
     const { title, subtitles, icon, isError, showContinueBtn } = props;
+    const [isWideScreen, setIsWideScreen] = useState(false);
+
+    useEffect(() => {
+        // Update the screen width state
+        const handleResize = () => setIsWideScreen(window.innerWidth > 768);
+        
+        // Set initial state
+        handleResize();
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     // Conditionally render content based on screen width
     const renderContent = () => {
-        if (window.innerWidth > 768) {
+        if (isWideScreen) {
             return (
                 <>
-                    <div className="MegaTip-icon">
-                        {icon}
-                    </div>
-                    <div className="MegaTip-content" >
+                    <div className="MegaTip-icon">{icon}</div>
+                    <div className="MegaTip-content">
                         <div className="Stack stackColumn" style={{ flexDirection: "column", padding: 0, alignItems: "stretch", justifyContent: "center" }}>
                             <div className="Stack-child" style={{ paddingTop: 8 }}>
                                 <div className="Heading s isWeak MegaTip-title">
@@ -39,9 +51,7 @@ const SEAlertMessage = (props) => {
                         <div className="Stack-child" style={{ paddingTop: 15 }}>
                             <div className="Stack stackRow" style={{ flexDirection: "row", padding: 0, alignItems: "center" }}>
                                 <div className="Stack-child" style={{ paddingLeft: 0 }}>
-                                    <div className="MegaTip-icon">
-                                        {icon}
-                                    </div>
+                                    <div className="MegaTip-icon">{icon}</div>
                                 </div>
                                 <div className="Stack-child" style={{ paddingLeft: 0 }}>
                                     <div className="Heading s isWeak MegaTip-title">
@@ -76,9 +86,9 @@ const SEAlertMessage = (props) => {
                     </div>
                 </div>
             </div>
-            {showContinueBtn && <ButtonLarge name="Continuer" uniqueId={`${title}-continue-btn`}/>}
+            {showContinueBtn && <ButtonLarge name="Continuer" uniqueId={`${title}-continue-btn`} />}
         </div>
     );
-}
+};
 
 export default SEAlertMessage;
