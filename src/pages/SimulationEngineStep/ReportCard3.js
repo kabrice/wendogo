ccc03818ab88e2ce6d6d84f93dd48222dd2646b1
Edgar KAMDEM <e.kamdem@pickup-services.com>
2024-11-30 19:36:40 +0100

    Add duplicate icon in ReportCard3

--- src/pages/SimulationEngineStep/ReportCard3.js
+++ src/pages/SimulationEngineStep/ReportCard3.js
@@ -319,12 +319,13 @@ const ReportCard3 = () => {
                         </div>
                         <div className="Tip" style={{marginBottom:'-5px'}}>
                             <span>
-                                Gagnez du temps : dupliquez
-                                {/* Duplicate icon SVG */}
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 90.7659 97.9493" version="1.1" className='Navbarstyles__MenuItemArrow'>
-                                    {/* SVG path content */}
-                                </svg>
-                                 et modifiez les bulletins pour les {user?.academicYearHeadDetails3.academicYearOrganization?.name?.toLowerCase()+'s'} similaires.
+                                Gagnez du temps : dupliquez <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"  viewBox="0 0 90.7659 97.9493" version="1.1" className='Navbarstyles__MenuItemArrow'>
+                                    <title>Shape</title>
+                                    <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                                        <g id="noun-duplicate-7205800" fill="currentcolor" fillRule="nonzero">
+                                            <path d="M21.2227,23.5081 L21.2227,11.8711 C21.2227,8.6055 22.5586,5.6328 24.7071,3.4844 C26.8594,1.3321 29.8282,0 33.0938,0 L78.8948,0 C82.1604,0 85.1331,1.3359 87.2815,3.4844 C89.4338,5.6367 90.7659,8.6055 90.7659,11.8711 L90.7659,62.5701 C90.7659,65.8357 89.43,68.8084 87.2815,70.9568 C85.1292,73.1091 82.1604,74.4412 78.8948,74.4412 L69.5432,74.4412 L69.5432,86.0782 C69.5432,89.3438 68.2073,92.3165 66.0588,94.4649 C63.9065,96.6172 60.9377,97.9493 57.6721,97.9493 L11.8711,97.9493 C8.6055,97.9493 5.6328,96.6134 3.4844,94.4649 C1.3321,92.3126 0,89.3438 0,86.0782 L0,35.3792 C0,32.1136 1.3359,29.1409 3.4844,26.9925 C5.6367,24.8402 8.6055,23.5081 11.8711,23.5081 L21.2227,23.5081 Z M69.5467,69.8521 L78.8983,69.8521 C80.8983,69.8521 82.7186,69.03179 84.0389,67.7115 C85.3592,66.3912 86.1795,64.5709 86.1795,62.5709 L86.1795,11.8719 C86.1795,9.8719 85.35919,8.0516 84.0389,6.7313 C82.7186,5.411 80.8983,4.5907 78.8983,4.5907 L33.0973,4.5907 C31.0973,4.5907 29.277,5.41101 27.9567,6.7313 C26.6325,8.0516 25.8161,9.8719 25.8161,11.8719 L25.8161,23.5089 L57.6751,23.5089 C60.9407,23.5089 63.9134,24.8448 66.0618,26.9933 C68.2141,29.1456 69.5462,32.1105 69.5462,35.38 L69.5467,69.8521 Z" id="Shape" /> </g>
+                                    </g>
+                                </svg> et modifiez les bulletins pour les {user?.academicYearHeadDetails3.academicYearOrganization?.name?.toLowerCase()+'s'} similaires.
                             </span>
                         </div>
                         <div className="DrawerAnimation" />
