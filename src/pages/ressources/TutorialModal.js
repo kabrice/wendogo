49d9674aefa3ce166da3165fbae524dfd8671f05
Edgar KAMDEM <e.kamdem@pickup-services.com>
2024-12-04 15:53:20 +0100

    fix text in tutorial

--- src/pages/ressources/TutorialModal.js
+++ src/pages/ressources/TutorialModal.js
@@ -20,7 +20,7 @@ const TutorialModal = ({ isOpen, onClose }) => {
                 </button>
                 <div className="text-center">
                     <h3 className="text-xl font-semibold mb-4">Bienvenue sur Wendogo!</h3>
-                    <p className="mb-6"> Pour mieux comprendre comment utiliser notre plateforme, nous vous recommandons de regarder notre tutoriel vidéo. </p> {/* Thumbnail Container */}
+                    <p className="mb-6"> Pour mieux comprendre comment utiliser notre plateforme, vous pouvez visionner notre tutoriel vidéo. </p> {/* Thumbnail Container */}
                     <div className="relative mb-6 group cursor-pointer"> <a href="https://www.youtube.com/watch?v=wUrgx3WLURI" target="_blank" rel="noopener noreferrer">
                                     {/* Video Thumbnail */}
                                     <img 
