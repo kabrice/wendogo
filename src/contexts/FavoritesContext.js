e733187e6fb3e47ace39804a2268803a957d9f53
Edgar KAMDEM <e.kamdem@pickup-services.com>
2025-07-17 02:07:56 +0200

    update FavoritesContext

--- src/contexts/FavoritesContext.js
+++ src/contexts/FavoritesContext.js
@@ -85,10 +85,10 @@ export const FavoritesProvider = ({ children }) => {
         method: isFavorite ? 'DELETE' : 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${session.accessToken}`,
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-          'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS'
+          'Authorization': `Bearer ${session.accessToken}`
+          // 'Access-Control-Allow-Origin': '*',
+          // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+          // 'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS'
         },
         credentials: 'include', // ✅ IMPORTANT: Inclure les cookies
         body: JSON.stringify({ program_id: programId }),
