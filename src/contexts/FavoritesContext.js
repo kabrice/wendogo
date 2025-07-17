d66c2e257dc46cfa6e8377f268f0abbb39293e49
Edgar KAMDEM <e.kamdem@pickup-services.com>
2025-07-17 03:19:12 +0200

    try to fix accesstoken error

--- src/contexts/FavoritesContext.js
+++ src/contexts/FavoritesContext.js
@@ -39,10 +39,7 @@ export const FavoritesProvider = ({ children }) => {
         method: 'GET',
         headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${session.accessToken}`,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-        'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS'
+        'Authorization': `Bearer ${session.accessToken}`
         }, 
         credentials: 'include', // ✅ IMPORTANT: Inclure les cookies
       });
